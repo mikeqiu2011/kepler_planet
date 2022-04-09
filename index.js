7f35d8362e719ba7501a002787b2a70c18e79d79
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-09 11:29:11 +0800

    only add to list if planet is habitable

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const { parse } = require('csv-parse');
 const fs = require('fs')
 
-const results = []
+const habitablePlanet = []
+
+const isHabitablePlanet = (planet) => {
+    return planet.koi_disposition === 'CONFIRMED'
+}
 
 fs.createReadStream('kepler_data.csv')
     .pipe(parse({ // readableStream.pipe(writebaseStream)
         comment: '#',
         columns: true
     }))
-    .on('data', (chunk) => {
-        results.push(chunk)
+    .on('data', (planet) => {
+        if (isHabitablePlanet(planet)) {
+            habitablePlanet.push(planet)
+        }
     })
     .on('error', (err) => {
         console.log(err);
     })
     .on('end', () => {
-        console.log(results); // now print the bytes array
+        console.log(habitablePlanet); // now print the bytes array
         console.log('done');
     })
 
--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 
 ## data source
     https://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=cumulative
+
+
+## vscode
+    cmd + shift + l: Select all occurrences of selected word in VSCode
