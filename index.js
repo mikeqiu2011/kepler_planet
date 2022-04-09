626ad3d1f3d988a1468758121ba8a67961cb7bce
mikeqiu2011 <mikeqiu2011@163.com>
2022-04-09 11:36:56 +0800

    further filter by size and temprature

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const habitablePlanet = []
 
 const isHabitablePlanet = (planet) => {
     return planet.koi_disposition === 'CONFIRMED'
+        && planet.koi_insol > 0.36 && planet.koi_insol < 1.11 // not too cold or too hot
+        && planet.koi_prad < 1.6  // not too big
 }
 
 fs.createReadStream('kepler_data.csv')
